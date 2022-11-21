9dba66d8a942f31e4a0d93c81fe4f59ff4a00289
MiloszSipka <milosz.sipka@gmail.com>
2022-11-21 13:56:19 +0000

    Test commit

--- BmiCalculator/src/BmiCalculator.java
+++ BmiCalculator/src/BmiCalculator.java
@@ -3,8 +3,9 @@ public class BmiCalculator {
 
 	public static void main(String[] args) {
 		System.out.println("This is new project");
-		
-
+		int a = 500;
+		System.out.println(a);
+		System.out.println("Test");
 	}
 
 }
