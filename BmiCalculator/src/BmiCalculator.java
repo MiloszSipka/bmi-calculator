b0b12d10cf3c0673ace2bd065f3147d7c35a0f67
MiloszSipka <milosz.sipka@gmail.com>
2022-11-22 14:00:35 +0000

    Creating GUI and implementing simple calculations

--- BmiCalculator/src/BmiCalculator.java
+++ BmiCalculator/src/BmiCalculator.java
@@ -1,11 +1,24 @@
-
+import javax.swing.JOptionPane;
+import java.math.*;
 public class BmiCalculator {
 
 	public static void main(String[] args) {
-		System.out.println("This is new project");
-		int a = 500;
-		System.out.println(a);
-		System.out.println("Test");
+	double height = Double.parseDouble(JOptionPane.showInputDialog("What's your height?" + " (In meters)"));
+	double weight = Double.parseDouble(JOptionPane.showInputDialog("What's your weight?" + " (In kilograms)"));
+	double bmi = weight / Math.pow(height, 2);
+	bmi = Math.round(bmi);
+	if(bmi<18.5) {
+		JOptionPane.showMessageDialog(null, "Your BMI is " + bmi + " and it falls within the underweight rangen");
+	} 
+	else if(bmi > 18.5 && bmi < 25){
+		JOptionPane.showMessageDialog(null, "Your BMI is " + bmi + " and it falls within the healthy weight range.");
+	}
+	else if(bmi > 25 && bmi < 30) {
+		JOptionPane.showMessageDialog(null, "Your BMI is " + bmi + " and it falls within the overweight range.");
+	}
+	else if(bmi > 30) {
+		JOptionPane.showMessageDialog(null, "Your BMI is " + bmi + " and it falls within the obesity range.");
+	}
 	}
 
 }
